6dd08ccafe1da3571930d0ae1b91a54e5d15290f
AJesusBarrosoMesa2 <abarmes1411@iescarrillo.es>
2025-05-28 11:06:11 +0200

    Cambios en la edicion de reseñas

--- src/app/pages/userreviews/userreviews.component.ts
+++ src/app/pages/userreviews/userreviews.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import {CustomlistResumeComponent} from '../../components/customlist-resume/customlist-resume.component';
+import {NgForOf, NgIf} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
+import {UserreviewsResumeComponent} from '../../components/userreviews-resume/userreviews-resume.component';
+import {UserreviewsService} from '../../services/userreviews.service';
 
 @Component({
   selector: 'app-userreviews',
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    ReactiveFormsModule,
+    UserreviewsResumeComponent
+  ],
   templateUrl: './userreviews.component.html',
   styleUrl: './userreviews.component.css'
 })
 export class UserreviewsComponent {
+
+  reviewList: any[] = [];
+
+
+  constructor(private userReviewService: UserreviewsService) {}
+
+  ngOnInit(): void {
+    this.loadUserReviews();
+  }
+
+  loadUserReviews(): void {
+    this.userReviewService.getAllUserReviews().subscribe((data) => {
+      this.reviewList = data;
+    });
+  }
 
 }
